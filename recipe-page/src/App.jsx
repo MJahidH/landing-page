92d7d6e1393e52bee3c9f6056b5509a7e3fa491f
MJahidH <m.jahid.h@hotmail.com>
2024-05-25 22:27:56 +0100

    instructions title removed

--- recipe-page/src/App.jsx
+++ recipe-page/src/App.jsx
@@ -1,14 +1,33 @@
 import { useState } from "react";
 import "./App.css";
+import foodImage from "./assets/image-omelette.jpeg";
+import data from "./assets/omelette-recipe-data.json";
 
 function App() {
   return (
     <>
       <div className="main-div">
         <div className="child">
-          <h1>This is where the image will go </h1> 
-          <h1>This is where the image will go </h1> 
-          </div>
+          <img className="food-image" src={foodImage}></img>
+          <h1 className="main-title">{data.title} </h1>
+          <p className="main-description">{data.mainDescription}</p>
+          <h1 className="prep-title">Preperation Time </h1>
+          <ul className="prep-list">
+            <li className="prep-bullet">
+              <span className="bold-bullet">Total: </span>
+              {data.preperationTime.total}
+            </li>
+            <li className="prep-bullet">
+              <span className="bold-bullet">Preperation: </span>
+              {data.preperationTime.preperation}
+            </li>
+            <li className="prep-bullet">
+              <span className="bold-bullet">Cooking: </span>
+              {data.preperationTime.cooking}
+            </li>
+          </ul>
+
+        </div>
       </div>
     </>
   );
